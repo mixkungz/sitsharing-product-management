a2cd0da19ef6c450a5af8ecd5065b9ba5a579192
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2020-02-15 02:31:00 +0700

    :refactor: Move the create user to be a register user in auth module

--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const snakeCaseKeys = require('snakecase-keys')
 
 const knex = require('../../../db')
-const { loginSchema } = require('./validates')
+const { loginSchema, createUserSchema } = require('./validates')
 
 const router = express.Router()
 
@@ -26,5 +27,31 @@ router.post('/login', (req, res) => {
       return res.status(500).send(err.errors)
     })
 })
+
+router.post('/register', (req, res) => {
+  createUserSchema.validate(req.body, { abortEarly: false })
+    .then(async() => {
+      const { password: plainTextPassword } = req.body
+      const password = bcrypt.hashSync(plainTextPassword, 10)
+      req.body.password = password
+      req.body.birthDate = new Date(req.body.birthDate)
+
+      req.body = snakeCaseKeys(req.body)
+      try {
+        const [userId] = await knex('users').insert(req.body)
+        const user = await knex('users').where('id', userId)
+
+        return res.status(201).send(user)
+      } catch (error) {
+        return res.status(500).send({
+          message: error.sqlMessage
+        })
+      }
+    })
+    .catch(err => {
+      return res.status(400).send(err.errors)
+    })
+})
+
 
 module.exports = router
--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -1,11 +1,8 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
-const snakeCaseKeys = require('snakecase-keys')
 
 const router = express.Router()
 
 const knex = require('../../../db')
-const { createUserSchema } = require('./validates')
 
 router.get('/', async (_, res) => {
   const users = await knex('users')
@@ -18,31 +15,6 @@ router.get('/:id', async (req, res) => {
   const user = await knex('users').where('id', id).first()
 
   return res.status(200).send(user)
-})
-
-router.post('/', (req, res) => {
-  createUserSchema.validate(req.body, { abortEarly: false })
-    .then(async() => {
-      const { password: plainTextPassword } = req.body
-      const password = bcrypt.hashSync(plainTextPassword, 10)
-      req.body.password = password
-      req.body.birthDate = new Date(req.body.birthDate)
-
-      req.body = snakeCaseKeys(req.body)
-      try {
-        const [userId] = await knex('users').insert(req.body)
-        const user = await knex('users').where('id', userId)
-
-        return res.status(201).send(user)
-      } catch (error) {
-        return res.status(500).send({
-          message: error.sqlMessage
-        })
-      }
-    })
-    .catch(err => {
-      return res.status(400).send(err.errors)
-    })
 })
 
 module.exports = router
