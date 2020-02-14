2b062cb428ef5957203a02882499c8cb93a3f738
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2020-02-14 18:04:25 +0700

    :sparkles: Can create new user

---
+++ src/modules/users/validates.js
@@ -0,0 +1,12 @@
+const yup = require('yup')
+
+module.exports = {
+  createUserSchema: yup.object().shape({
+    username: yup.string().min(6).required(),
+    password: yup.string().required().matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Password must has minimum eight characters, at least one letter and one number"),
+    firstName: yup.string().min(3).required(),
+    lastName: yup.string().min(3).required(),
+    birthDate: yup.date().required(),
+    sex: yup.string().matches(/(male|female)/).required()
+  })
+}
