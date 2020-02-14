b110f42a96c15a0ede03b97e5784f13c9648cb3f
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2020-02-14 14:09:40 +0700

    :tada: Initial nodejs server

---
+++ app.js
@@ -0,0 +1,24 @@
+const cors = require('cors')
+const helmet = require('helmet')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// ----------------------
+//     INITIAL SERVER
+// ----------------------
+app.use(helmet())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// ----------------------
+
+app.listen(port, () => {
+  console.log('Express server listening on port ' + port)
+})
+
+app.get('/api/v1', (_, res) => {
+  return res.status(200).send({ message: 'API Work !'})
+})
