c7ac29bfda5460bd55fe8f049f0793e34c3db8d2
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2020-02-15 11:26:46 +0700

    :mute: Removing log

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -8,9 +8,7 @@ verifyToken = (req, res, next) => {
 
   const [_, token] = req.headers['authorization'].split(' ')
   if (token === '') return res.status(403).send({ auth: false, message: 'No token provided.' })
-  console.log(token)
   jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-    console.log('err', err)
     if (err)
       return res.status(500).send({ auth: false, message: 'Failed to authenticate expired token.' })
     req.userId = decoded.userId
